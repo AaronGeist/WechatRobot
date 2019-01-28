9cf4e26805f18c45ca14dca248b8025bc773af1e
maojun.zy <maojun.zy@alipay.com>
2019-01-28 14:39:40 +0800

    Add log for token

--- biz/token.py
+++ biz/token.py
@@ -16,13 +16,17 @@ class Token:
     def fetch(self):
         token = self.cache.get(self.TOKEN_KEY)
         if token is None:
+            print("None token, need to fetch")
             # refresh token
             response = HttpUtils.get(url=self.URL_TEMPLATE.format(Config.get("app_id"), Config.get("app_secret")),
                                      return_raw=True)
             print(response.text)
+
+            print("Fetch done, " + str(response.text))
             self.cache.set_with_expire(self.TOKEN_KEY, token, self.EXPIRE_TIME)
 
         return token
+
 
 if __name__ == "__main__":
     token = Token()
